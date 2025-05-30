13b74be4be9bf6b2d753aa747ea09aed23caa197
govind0411 <govindmishra0411@gmail.com>
2025-05-31 00:38:47 +0530

    Add files via upload

---
+++ task2_greeting.py
@@ -0,0 +1,6 @@
+first_name=input("Enter your first Name: ")
+last_name=input("Enter your last Name: ")
+
+full_name=first_name + "  "+ last_name
+
+print(f"Hello,{full_name}! Welcome to the Python world!")
